4c97a69db3488da0e70c31bc18109da4da612181
arnu515 <arnu5152@gmail.com>
2021-01-10 20:59:21 +0530

    :bug:

--- tests/test-config.ts
+++ tests/test-config.ts
@@ -1,5 +1,5 @@
 export const COUCH_URI =
     process.env.COUCHDB_URL || "http://username:password@localhost:5984";
 
-export const COUCH_DB_1 = process.env.COUCH_DB || "test-db";
-export const COUCH_DB_2 = process.env.COUCH_DB || "test-db-2";
+export const COUCH_DB_1 = process.env.COUCH_DB_1 || "test-db";
+export const COUCH_DB_2 = process.env.COUCH_DB_2 || "test-db-2";
